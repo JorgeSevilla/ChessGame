fcb8aec13717b60cd5719956ca5a9e25b3c66725
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-01-18 22:05:48 -0300

    Create a class for Piece

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,5 +1,41 @@
 package pieces;
 
-public class Piece {
+import java.util.ArrayList;
+
+public abstract class Piece implements Cloneable{
+	
+	private int color;
+	private String id = null;
+	private String path;
+	protected ArrayList<Cell> possiblemoves = new ArrayList<Cell>();
+	protected abstract ArrayList<Cell> move(cell pos[][], int x, int y);
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	public void setColor(int c) {
+		this.color = c;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public int getColor() {
+		return this.color;
+	}
+	
+	public Piece getCopy() throws CloneNotSupportedException{
+		return (Piece) this.clone();
+	}
 
 }
