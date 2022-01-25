80b48af8f74b9c25cbe5bf88a467104a90b8700e
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-01-24 23:31:36 -0300

    Create a Pawn class

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,5 +1,56 @@
 package pieces;
 
-public class Pawn {
+import java.util.ArrayList;
 
+public class Pawn extends Piece {
+
+	public Pawn(String i, String p, int c) {
+		setId(c);
+		setPath(p);
+		setColor(i);
+
+	}
+
+	public ArrayList<Cell> move(Cell state[][], int x, int y){
+		
+		possibleMoves.clear();
+		
+		if(getColor == 0) {
+			if(x == 0) {
+				return possibleMoves;
+				if(state[x - 1][y].getPiece() == null) {
+					possibleMoves.add(state[x - 1][y]);
+					if(x == 6) {
+						if(state[4][y].getPiece() == null) {
+							possibleMoves.add(state[4][y]);
+						}
+					}
+					if((y > 0 ) && (state[x - 1 ][y - 1].getPiece() != null) && (state[x - 1][y - 1].getPiece().getColor() != this.getColor())
+						possibleMoves.add(state[x - 1][y - 1]);
+					if((y < 7 ) && (state[x - 1 ][y + 1].getPiece() != null) && (state[x - 1][y + 1].getPiece().getColor() != this.getColor())
+						possibleMoves.add(state[x - 1][y - 1]);
+				}
+				
+				else {
+					if(x == 8)
+						return possibleMoves;
+					if(state[x + 1][y].getPiece() == null) {
+						possibleMoves.add(state[x + 1][y]);
+						if(x == 1) {
+							if(state[3][y].getPiece() == null)
+								possibleMoves.add(state[3][y]);
+						}
+					}
+					
+					if((y > 0 ) && (state[x + 1 ][y + 1].getPiece() != null) && (state[x + 1][y + 1].getPiece().getColor() != this.getColor())
+							possibleMoves.add(state[x + 1][y - 1]);
+						if((y < 7 ) && (state[x + 1 ][y + 1].getPiece() != null) && (state[x + 1][y + 1].getPiece().getColor() != this.getColor())
+							possibleMoves.add(state[x + 1][y + 1]);
+					
+				}
+				return possibleMoves;
+				
+			}
+		}
+	}
 }
--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -7,7 +7,7 @@ public abstract class Piece implements Cloneable{
 	private int color;
 	private String id = null;
 	private String path;
-	protected ArrayList<Cell> possiblemoves = new ArrayList<Cell>();
+	protected ArrayList<Cell> possibleMoves = new ArrayList<Cell>();
 	protected abstract ArrayList<Cell> move(cell pos[][], int x, int y);
 	
 	public void setId(String id) {
