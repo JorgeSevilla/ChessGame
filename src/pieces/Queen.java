1b934f00105b398d1775eba867cbd407157b74f3
Jorge Sevilla <jorge.garcia@compasso.com.br>
2022-02-01 22:37:42 -0300

    Created class Queen

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -1,5 +1,122 @@
 package pieces;
 
-public class Queen {
+import java.util.ArrayList;
 
+public class Queen extends Piece {
+
+	public Queen(String i, String p, int c) {
+		
+		setId(i);
+		setPath(p);
+		setColor(c);
+	}
+	
+	public ArrayList<Cell> move(Cell state[][], int x, int y){
+		
+		possibleMoves.clear(0);
+		
+		int tempx = x - 1;
+		while(tempx >= 0) {
+			if(state[tempx][y].getPiece() == null)
+				possibleMoves.add(state[tempx][y]);
+			else if(state[tempx][y].getPiece().getColor() == this.getColor())
+				break;
+			else {
+				possibleMoves.add(state[tempx][y]);
+			}
+			tempx--;
+		}
+		
+		int tempx = x + 1;
+		while(tempx < 8) {
+			if(state[tempx][y].getPiece() == null)
+				possibleMoves.add(state[tempx][y]);
+			else if(state[tempx][y].getPiece().getColor() == this.getColor())
+				break;
+			else {
+				possibleMoves.add(state[tempx][y]);
+			}
+			tempx++;
+		}
+		
+		//Checking possible moves in horizontal Direction
+		int tempy = y - 1;
+		while(tempy >= 0) {
+			if(state[x][tempy].getPiece() == null)
+				possibleMoves.add(state[x][tempy]);
+			else if(state[x][tempy].getPiece().getColor() == this.getColor())
+				break;
+			else {
+				possibleMoves.add(state[x][tempy]);
+			}
+			tempy--;
+		}
+		
+		int tempy = y + 1;
+		while(tempy >= 0) {
+			if(state[x][tempy].getPiece() == null)
+				possibleMoves.add(state[x][tempy]);
+			else if(state[x][tempy].getPiece().getColor(0)== this.getColor())
+				break;
+			else {
+				possibleMoves.add(state[x][tempy]);
+			}
+			tempy++;
+		}
+		
+		//Checking for possible moves in diagonal direction
+		tempx = x + 1; tempy = y - 1;
+		while(tempx < 8  && tempy >= 0) {
+			if(state[tempx][tempy].getPiece() == null)
+				possibleMoves.add(state[tempx][tempy]);
+			else if(state[tempx][tempy].getPiece.getColor() == this.getColor())
+					break;
+			else {
+				possibleMoves.add(state[tempx][tempy]);
+			}
+			tempx++;
+			tempy--;
+		}
+		
+		tempx = x - 1; tempy = y + 1;
+		while(tempx >= 0 && tempy < 8) {
+			if(state[tempx][tempy].getPiece() == null)
+				possibleMoves.add(state[tempx][tempy]);
+			else if(state[tempx][tempy].getPiece.getColor() == this.getColor())
+					break;
+			else {
+				possibleMoves.add(state[tempx][tempy]);
+			}
+			tempx--;
+			tempy++;
+		}
+		
+		tempx = x - 1; tempy = y - 1;
+		while(tempx >= 0 && tempy >= 0) {
+			if(state[tempx][tempy].getPiece() == null)
+				possibleMoves.add(state[tempx][tempy]);
+			else if(state[tempx][tempy].getPiece.getColor() == this.getColor())
+					break;
+			else {
+				possibleMoves.add(state[tempx][tempy]);
+			}
+			tempx--;
+			tempy--;
+		}
+		
+		tempx = x + 1; tempy = y + 1;
+		while(tempx < 8 && tempy < 8) {
+			if(state[tempx][tempy].getPiece() == null)
+				possibleMoves.add(state[tempx][tempy]);
+			else if(state[tempx][tempy].getPiece.getColor() == this.getColor())
+					break;
+			else {
+				possibleMoves.add(state[tempx][tempy]);
+			}
+			tempx++;
+			tempy++;
+		}
+		
+		return possibleMoves;
+	}
 }
